2f054954a9dd49bda6c68c63bca83a2cbaa34849
wave0s <zwave00s@gmail.com>
2025-08-02 21:43:41 +0800

    base8.2 - sign operator

--- src/index.js
+++ src/index.js
@@ -22,4 +22,35 @@ var screen = document.querySelector('#screen');
     function pow() {
         screen.value = Math.pow(screen.value, 2);
     }
-    /// Main algo -notation func
+    //// Pos-Negative toggle btn 
+    function toggleSign() {
+    if (screen.value === '0' || screen.value === '') return;
+
+    /// checks the if last number is valid 
+    const lastNumberMatch = screen.value.match(/([-+]?\d*\.?\d+)$/);
+
+    if (lastNumberMatch) {
+        /// this smwhat ensures na once you click the sign button 
+        const lastNumber = lastNumberMatch[1];
+        const beforeLastNumber = screen.value.slice(0, screen.value.lastIndexOf(lastNumber));
+
+        let newNumber;
+        //gna check nya if the input is either positive/negative then changes its sign after 
+        if (lastNumber.startsWith('-')) {
+            newNumber = lastNumber.slice(1);
+        } else {
+            newNumber = '-' + lastNumber;
+        }
+
+        screen.value = beforeLastNumber + newNumber;
+    } else {
+        /// safety net if toggle algo cnat find valid num
+        if (screen.value.startsWith('-')) {
+            screen.value = screen.value.slice(1);
+        } else {
+            screen.value = '-' + screen.value;
+        }
+    }
+}
+/// history **
+/// Main algo -notation func
